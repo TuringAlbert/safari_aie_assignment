371940e3f3fa7a1cbd3056f8b82010c1ec3bc78c
turingalbert <juyoungkim814@gmail.com>
2023-05-14 21:36:30 +0900

    grid complete 0514 jy

--- circle_of_life_jy.py
+++ circle_of_life_jy.py
@@ -8,33 +8,64 @@ def print_TODO(todo):
     
 class CircleOfLife:
     def __init__(self, world_size, num_zebras, num_lions):
-        self.occupancy = [[False for _ in range(world_size)]]
+        self.occupancy = [[False for _ in range(world_size)]
+                          for _ in range(world_size)]
         print_TODO('get random empty coordinates')
         self.zebras = [Animal(0,0) for _ in range(num_zebras)]
         self.lions = [Animal(0,0) for _ in range(num_lions)]
+        self.timestep = 0
+        self.world_size = world_size + 1
         print('Welcome to AIE Safari!')
         print(f'\tworld size = {world_size}')
         print(f'\tnumber of zebras = {len(self.zebras)}')
         print(f'\tnumber of lions = {len(self.lions)}')
 
-    def display():
-        print(f'Clock: ')
-        top_coord_str = " ".join([f'{coord}' for coord in range(20)])
-        print(top_coord_str)
+    def display(self):
         print("Welcome to Safari!")
-        print(" 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 ")
-        print("----------------------------------------------------")
+        print(f'time step = {self.timestep}')
+        # 상단 숫자 및 Clock Timestep
+        print(' ', end = '  ')
+        for i in range(1, self.world_size):
+            if i == 20:
+                print(f'{i:2} time step = {self.timestep}')
+            else:
+                print(f'{i:2}', end=' ')
         
-        # os.system('cls') ##전부 사라지게 하는 코드
+        # 상단 격자
+        print('   ', end = '  ')
+        top_coord_str = "-".join([ "-" for coord in range(31)])
+        print(top_coord_str)
+            
+        # 좌측 숫자 및 좌측/우측 격자 
+        for i in range(1, self.world_size):
+            print(f'{i:2}', end= '  ')
+            print(f'{"|":2}', end= '  ')
+            for _ in range(1, self.world_size):
+                print('0', end='  ')
+            print(f'{"|":2}', end= '  ')
+            print() #enter
+        
+        # 하단 격자
+        print('   ', end = '  ')
+        top_coord_str = "-".join([ "-" for coord in range(31)])
+        print(top_coord_str)
+        
+        # board = [[0]*self.world_size for _ in range(self.world_size)]
+        
+        # top_coord_str = " ".join([f'{coord}' for coord in range(self.world_size)]) #len(self.grid)
+        # print(top_coord_str)
+        
+        
+        # os.system('cls') ##input 이후 전부 사라지게 하는 코드
         
         # for i in range(20):
         #     print("--")
         grid = np.zeros((20,20), dtype= int)
-        print(grid)
+        # print(grid)
         grid = [[0 for _ in range(20)] for _ in range(20)]
         for i in range(20):
             grid.append(i + 1)
-        print(grid)
+        # print(grid)
 
             
     
@@ -50,8 +81,23 @@ class CircleOfLife:
             print_TODO('get empty neighbor')
             direction = 'left'
             zebra.move(direction)
+        for lion in self.lions:
+            print_TODO('get neighboring zebras')
+            print_TODO('move to zebra if found, else move to empty')
+            print_TODO('get empty neighbor')
+            direction = 'left'
+            lion.move(direction)
     
+    def step_breed(self):
+        print_TODO('step_breed()')
+        for animal in self.zebras + self.lions:
+            print_TODO('get empty neighbor')
+            x, y = 0, 0
+            animal.breed(x, y)
             
 if __name__ == '__main__':
-    safari = CircleOfLife(5,5,2)
-    CircleOfLife.display()
+    safari = CircleOfLife(20,100,5) #worldsize, zebras, lions
+    safari.display()
+    
+    # safari.step_move()
+    # safari.step_breed()
