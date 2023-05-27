db5e2dfcf8fbca4d143b918dbc9e266544b5c43e
haooiz <hannna260@gmail.com>
2023-05-28 01:04:53 +0900

    수정(에러)

--- animal_nk.py
+++ animal_nk.py
@@ -8,86 +8,127 @@ class Animal:
         self.x = x
         self.y = y
         self.age = 0
-        # self.hp = 3 # 동물의 생명력
+        self.hp = 3 # 동물의 생명력
         
     def move_to(self, grid, target) -> bool:
         ''' target can be ., L, or Z
-        returns True if moved
-        '''
+            returns True if moved'''
         neighbors = self.get_neighbors(grid, target)
         if len(neighbors) > 0:
             grid[self.y][self.x] = Empty(self.y, self.x)
             chosen_neighbor = random.choice(neighbors)
             self.y, self.x = chosen_neighbor
+            grid[self.y][self.x].hp = 0
             grid[self.y][self.x] = self
             return True
         else:
             return False
-        
-    def breed(self, y, x):
-         print(f'breed for {y}, {x}. <<<NOT IMPLEMENTED YET>>>')
      
+    def breed(self, grid):
+         print(f'breed for {y}, {x}. <<<NOT IMPLEMENTED YET>>>')
+         child = self.__class__(self.y, self.x)
+         child.move_to(grid, target='.')
+         grid[self.y][self.x] = self
+
     def get_neighbors(self, grid, target):
         '''target can be ., L, or Z
-        returns a list of coordinates
-        '''
+            returns a list of coordinates'''
         world_height = len(grid)
         world_width = len(grid[0])
-        x, y = self.x, self.y
+        y, x = self.y, self.x
         neighbors = []
         neighbors.append([y - 1, x])
         neighbors.append([y + 1, x])
         neighbors.append([y, x - 1])
         neighbors.append([y, x + 1])
         neighbors_valid = [neighbor for neighbor in neighbors
-                        if neighbor[0] >= 0 #x가 0보다 같거나 커야 한다
-                        and neighbor[0] < world_width # x가 world_width 보다 작아야 한다
-                        and neighbor[1] >= 0 #y가 0보다 같거나 커야 한다
-                        and neighbor[1] < world_height
-                        and str(grid[neighbor[1]][neighbor[0]]) == target] #y가 world_width 보다 작아야 한다
+                           if neighbor[0] >= 0 #x가 0보다 같거나 커야 한다
+                           and neighbor[0] < world_width # x가 world_width 보다 작아야 한다
+                           and neighbor[1] >= 0 #y가 0보다 같거나 커야 한다
+                           and neighbor[1] < world_height
+                           and str(grid[neighbor[0]][neighbor[1]]) == target] #y가 world_width 보다 작아야 한다
         print("neighbors : ", neighbors_valid)
         return neighbors_valid
 
-class Zebra(Animal):
-    def __str__(self) -> str:
-        return 'Z'
-    
-    def breed(self):
-        if self.age >= 3:
-            neighbors = self.get_neighbors(grid, ".")
-            if len(neighbors) > 0:
-                chosen_neighbor = random.choice(neighbors)
-                x, y = chosen_neighbor
-                new_zebra = Zebra(x, y)
-                return new_zebra
-        return None
-        
-    def move(self, grid):
-        self.move_to(grid, target=".")
+    def is_ready_to_breed(self):
+        return self.age != 0 and self.age % self.breed_interval == 0
+
 
 class Empty(Animal):
     def __str__(self) -> str:
         return '.'
 
+# class Zebra(Animal):
+#     def __str__(self) -> str:
+#         return 'Z'
+    
+    # def breed(self):
+    #     if self.age >= 3:
+    #         neighbors = self.get_neighbors(grid, ".")
+    #         if len(neighbors) > 0:
+    #             chosen_neighbor = random.choice(neighbors)
+    #             x, y = chosen_neighbor
+    #             new_zebra = Zebra(x, y)
+    #             return new_zebra
+    #     return None
+        
+    # def move(self, grid):
+    #     self.move_to(grid, target=".")
+
+    # def is_ready_to_breed(self):
+    #     return self.age != 0 and self.age % 3 == 0
+
+class Zebra(Animal):
+    def __str__(self) -> str:
+        return 'Z'
+
+    def move(self, grid):
+        neighbors = self.get_neighbors(grid, '.')
+        if len(neighbors) > 0:
+            grid[self.y][self.x] = Empty(self.y, self.x)
+            chosen_neighbor = random.choice(neighbors)
+            self.y, self.x = chosen_neighbor
+            grid[self.y][self.x] = self
+            self.age += 1
+            if self.is_ready_to_breed():
+                self.breed(grid)
+        else:
+            self.age += 1
+
+    def breed(self, grid):
+        child_coords = self.get_neighbors(grid, '.')
+        if len(child_coords) > 0:
+            child_y, child_x = random.choice(child_coords)
+            child = self.__class__(child_y, child_x)
+            grid[child_y][child_x] = child
+
+    def is_ready_to_breed(self):
+        return self.age != 0 and self.age % 3 == 0
+
+
 class Lion(Animal):
     def __str__(self) -> str:
         return 'L'
 
-    def breed(self):
-        if self.age >= 8:
-            neighbors = self.get_neighbors(grid, ".")
-            if len(neighbors) > 0:
-                chosen_neighbor = random.choice(neighbors)
-                x, y = chosen_neighbor
-                new_lion = Lion(x, y)
-                return new_lion
-        return None
+    # def breed(self):
+    #     if self.age >= 8:
+    #         neighbors = self.get_neighbors(grid, ".")
+    #         if len(neighbors) > 0:
+    #             chosen_neighbor = random.choice(neighbors)
+    #             x, y = chosen_neighbor
+    #             new_lion = Lion(x, y)
+    #             return new_lion
+    #     return None
     
     def move(self, grid):
         print(f'before: {self.x=}, {self.y=}')
-        hunt_is_successful = self.move_to(grid, target="Z")
+        hunt_is_successful = self.move_to(grid, target='Z')
         if hunt_is_successful:
             self.hp = 3
         else:
-            self.move_to(grid, target=".")
+            self.move_to(grid, target='.')
+            self.hp -= 1
         print(f'after: {self.x=}, {self.y=}')
+
+    def is_ready_to_breed(self):
+        return self.age != 0 and self.age % 8 == 0
--- circle_of_life_nk.py
+++ circle_of_life_nk.py
@@ -1,5 +1,5 @@
-from animal_jy_nk import Zebra, Lion, Animal, Empty
-import numpy as np
+from animal_jy_nk import Zebra, Lion, Empty
+# import numpy as np
 import os
 from utils import print_TODO
 import random
@@ -11,7 +11,6 @@ class CircleOfLife:
                                  for x in range(self.world_size)]
         random.seed(0)
         zebra_coords, lion_coords = self.get_random_coords(num_zebras, num_lions)
-        
         for y, x in zebra_coords:
             self.grid[y][x] = Zebra(y,x)
         for y, x in lion_coords:
@@ -19,7 +18,7 @@ class CircleOfLife:
         self.timestep = 0
         
         # self.update_grid()
-        self.timestep = 0
+        # self.timestep = 0
         print('Welcome to AIE Safari!')
         print(f'\tworld size = {world_size}')
         print(f'\tnumber of zebras = {num_zebras}')
@@ -34,43 +33,98 @@ class CircleOfLife:
         return zebra_coords, lion_coords
 
     def display(self):
-            # self.update_grid()
-            # 상단 숫자 및 Clock Timestep
-            print('     ', end = '  ')
-            for i in range(1, len(self.grid)+1):
-                if i == len(self.grid):
-                    print(f'{i:2} time step = {self.timestep}')
-                else:
-                    print(f'{i:2}', end=' ')
+        print(f'Clock: {self.timestep}')
+        for row in self.grid:
+            buffer = [str(animal) for animal in row]
+            print(' '.join(buffer))
+        key = input('Enter [q] to quit:')
+        if key == 'q':
+            exit()
 
-            # 상단 격자
-            print('   ', end = '  ')
-            top_coord_str = "-".join([ "-" for coord in range(len(self.grid))])
-            print(top_coord_str)
-            
-            for row, line in enumerate(self.grid):
-                buffer = [str(animal) for animal in line]
-                print(f'{row:2}' + ' '.join(buffer))
-          
-            # for zebra in self.zebras:
-            #     self.grid[zebra.y][zebra.x] = 'Z'
-            # for lion in self.lions:
-            #     self.grid[lion.y][lion.x] = 'L'
-            # print("self.grid : ", self.grid)
-            # 내부 값, 좌측 숫자 및 좌측/우측 격자 
-            for i in range(0, len(self.grid)):
-                print(f'{i:2}', end= '  ')
-                # print(f'{"|":2}', end= '  ')
+    # def display(self):
+    #     if os.name == 'nt':
+    #         os.system('cls')
+    #     else:
+    #         os.system('clear')
+    #     print(f'Clock: {self.timestep}')
+    #     top_coord_str = ' '.join([f'{coord}' for coord in range(len(self.grid))])
+    #     print(' ' + top_coord_str)
+    #     for row, line in enumerate(self.grid):
+    #         buffer = [str(animal) for animal in line]
+    #         print(f'{row:2} ' + ' '.join(buffer))
+    #     key = input('enter [q] to quit:')
+    #     if key == 'q':
+    #         exit()
 
-            # 하단 격자
-            # print('   ', end = '  ')
-            # top_coord_str = "-".join([ "-" for coord in range(31)])
-            # print(top_coord_str)
+    # def display(self):
+    #     self.update_grid()
+    #     print('     ', end = '  ')
+    #     for i in range(1, len(self.grid)+1):
+    #         if i == len(self.grid):
+    #             print(f'{i:2} time step = {self.timestep}')
+    #         else:
+    #                 print(f'{i:2}', end=' ')
+
+    #     # 상단 격자
+    #     print('   ', end = '  ')
+    #     top_coord_str = "-".join([ "-" for coord in range(len(self.grid))])
+    #     print(top_coord_str)
             
-            key = input('enter [q] to quit:')
-            # os.system('clear')
-            if key == 'q':
-                exit()
+    #     for row, line in enumerate(self.grid):
+    #         buffer = [str(animal) for animal in line]
+    #         print(f'{row:2}' + ' '.join(buffer))
+      
+        # for zebra in self.zebras:
+        #     self.grid[zebra.y][zebra.x] = 'Z'
+        # for lion in self.lions:
+        #     self.grid[lion.y][lion.x] = 'L'
+        # print("self.grid : ", self.grid)
+        # 내부 값, 좌측 숫자 및 좌측/우측 격자 
+        # for i in range(0, len(self.grid)):
+        #     print(f'{i:2}', end= '  ')
+            # print(f'{"|":2}', end= '  ')
+        # 하단 격자
+        # print('   ', end = '  ')
+        # top_coord_str = "-".join([ "-" for coord in range(31)])
+        # print(top_coord_str)
+        
+        # key = input('enter [q] to quit:')
+        # os.system('clear')
+        # if key == 'q':
+        #     exit()
+
+    def step_move(self):
+        animals = [animal for line in self.grid for animal in line
+                   if not isinstance(animal, Empty)]
+        for animal in animals:
+            if animal.hp != 0:
+                animal.move(self.grid)
+
+    def clear_bodies(self):
+        for y, line in enumerate(self.grid):
+            for x, animal in enumerate(line):
+                if isinstance(animal, Animal):
+                    animal.age += 1
+                    if animal.hp == 0:
+                        self.grid[y][x] = Empty(y, x)
+
+    def step_breed(self):
+        # return
+        animals = [animal for line in self.grid for animal in line
+                   if not isinstance(animal, Empty)
+                   and animal.is_ready_to_breed()]
+        for animal in animals:
+            animal.breed(self.grid)
+
+    def step_hunger(self):
+        # return
+        lions = [lion for line in self.grid for lion in line
+                 if isinstance(lion, Lion)]
+        for lion in lions:
+            if lion.age >= 3:
+                lion.hp -= 1
+                if lion.hp == 0:
+                    self.grid[lion.y][lion.x] = Empty(lion.y, lion.x)
 
     def update_grid(self):
         self.grid = [["." for _ in range(self.world_size)]
@@ -79,42 +133,20 @@ class CircleOfLife:
             self.grid[animal.y][animal.x] = "Z"
         for animal in self.lions:
             self.grid[animal.y][animal.x] = "L"
-        
-    def step_move(self):
-        animals = [animal for line in self.grid for animal in line
-                   if not isinstance(animal, Empty)]
-        for animal in animals:
-            animal.move(self.grid)
             
-
-    def step_breed(self):
-        return
-        animals = [animal for line in self.grid for animal in line
-                if not isinstance(animal, Empty)]
-        for animal in animals:
-            animal.breed(self.grid)
-
-    def step_hunger(self):
-        return
-        for lion in self.lions:
-            if lion.age >= 3:
-                lion.hunger_count += 1
-                if lion.hunger_count >= 3:
-                    self.lions.remove(lion)
-            
-    def run(self, num_timesteps=100):
+    # def run(self, num_timesteps=100):
+    def run(self, num_steps):
         self.display()
-        for _ in range(num_timesteps):
+        for _ in range(num_steps):
             self.timestep += 1
             self.step_move()
-            # self.display()
-            self.step_breed()
-            # self.display()
-            self.step_hunger()
             self.display()
+            self.step_breed()
+            self.display()
+            self.step_hunger()
+            # self.display()
+            self.clear_bodies()
             
 if __name__ == '__main__':
     safari = CircleOfLife(5,2,1) #worldsize, zebras, lions
     safari.run(100)
-
-print("new05")
